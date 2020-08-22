03b16c38c4ce5f362b5366a2869840f266351afb
tolmachev nikita <toniktoo@mail.ru>
2020-08-23 00:23:03 +0300

    Updates

---
+++ precache-manifest.2e12add11969f3ba57f26f82633385ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b03e633e9cee73d98e780314112ff8",
+    "url": "/bankRussia/index.html"
+  },
+  {
+    "revision": "e49b8f244494427c481a",
+    "url": "/bankRussia/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "9b7ab14fd04218699ca5",
+    "url": "/bankRussia/static/css/main.ac0c64d7.chunk.css"
+  },
+  {
+    "revision": "e49b8f244494427c481a",
+    "url": "/bankRussia/static/js/2.28bb4793.chunk.js"
+  },
+  {
+    "revision": "73cd6530ecef3810103527d403b9d1d5",
+    "url": "/bankRussia/static/js/2.28bb4793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b7ab14fd04218699ca5",
+    "url": "/bankRussia/static/js/main.021222bb.chunk.js"
+  },
+  {
+    "revision": "41e01d7044d5cf7e8af9",
+    "url": "/bankRussia/static/js/runtime-main.f5681c61.js"
+  }
+]);
